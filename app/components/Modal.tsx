49f769e20271a4c8bd3aa0853879d7201ffc4776
danilo-timacum <danilo@timacum.com>
2023-07-06 17:01:30 +0200

    modal notes

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,27 +1,32 @@
 import { createPortal } from 'react-dom';
 import { StyledModal } from './ComponentStyles/Modal.styled';
 type Props = {
-  children: JSX.Element | JSX.Element[] | React.ReactNode;
-  title: string;
-  setShowModal: (showModal: boolean) => void;
+	children: JSX.Element | JSX.Element[] | React.ReactNode;
+	title: string;
+	setShowModal: (showModal: boolean) => void;
 };
+
+// Good job, just two more things to handle:
+// 1. allow modal to be closed by clicking on backdrop (overlay) but make it conditional and true by default
+// 2. allow diferent domNodes for portal to be passed, make it so default is document.body
+
 const Modal = ({ children, title, setShowModal }: Props) => {
-  return createPortal(
-    <StyledModal>
-      <div className="overlay">
-        <div className="modal">
-          <div className="title-and-close">
-            <strong>{title}</strong>
-            <button className="close-modal" onClick={() => setShowModal(false)}>
-              x
-            </button>
-          </div>
-          <div>{children}</div>
-        </div>
-      </div>
-    </StyledModal>,
-    document.getElementById('portal')!
-  );
+	return createPortal(
+		<StyledModal>
+			<div className='overlay'>
+				<div className='modal'>
+					<div className='title-and-close'>
+						<strong>{title}</strong>
+						<button className='close-modal' onClick={() => setShowModal(false)}>
+							x
+						</button>
+					</div>
+					<div>{children}</div>
+				</div>
+			</div>
+		</StyledModal>,
+		document.getElementById('portal')!
+	);
 };
 
 export default Modal;
